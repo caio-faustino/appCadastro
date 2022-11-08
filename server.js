87e57dc2891059a14d2a95aa1f3add677ab6c568
caio-faustino <caioexfaustino.pro@gmail.com>
2022-11-08 14:55:57 -0300

    connection with mongoDB

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.get('/', function(req,res) {
     res.json({message:'Hello Turma'});
 });
 
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://127.0.0.1/test',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    family: 4,
+},function (err) {
+    if(err){console.log(err)    
+}else{
+    console.log('conectado ao mongoDB')
+}
+})
 
 app.listen(5000, function() {
     console.log('Server on port 5000')
