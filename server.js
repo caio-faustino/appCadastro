9ae1d0d770e1d87d35165e151a6ab1eac4a40aca
caio-faustino <caioexfaustino.pro@gmail.com>
2022-11-08 14:43:24 -0300

    update defaut estructure

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get('/', function(req,res) {
     res.json({message:'Hello Turma'});
 });
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.listen(5000, function() {
     console.log('Server on port 5000')
