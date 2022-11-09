e440679a08656af887d309745074945fbda6d8c4
caio-faustino <caioexfaustino.pro@gmail.com>
2022-11-09 13:33:31 -0300

    add default estructure front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h>JCF Distribuição SIstema</h>
+        <h1>JCF Distribuição SIstema</h1>
       </header>
     </div>
   );
