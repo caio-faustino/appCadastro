8afb5d79a5a2c2af6786903f9214c3b02da9e521
caio-faustino <caioexfaustino.pro@gmail.com>
2022-11-08 22:03:43 -0300

    add functions CRUD

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -2,16 +2,16 @@ const Usuario = require('../models/usuario.model');
 
 
 module.exports = {
-  index(req,res) {
-    res.json({message:'Hello Turma'})
+  async index(req,res) {
+    const user = await Usuario.find();
+    res.json(user)
   },
 
   async create(req,res){
     const {nome_usuario, email_usuario, tipo_usuario, senha_usuario} = req.body;
-
     let data = {};
-
     let user = await Usuario.findOne({email_usuario});
+
     if(!user){
         data = {nome_usuario, email_usuario, tipo_usuario, senha_usuario};
         user = await Usuario.create(data);
@@ -19,5 +19,10 @@ module.exports = {
     }else{
         return res.status(500).json(user);
     }
-  }
+  },
+  async details(req,res) {
+    const {_id} = req.params;
+    const user = await Usuario.findOne({_id});
+    res.json(user);
+  },
 }
