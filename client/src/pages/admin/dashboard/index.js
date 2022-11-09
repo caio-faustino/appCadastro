53f299045723084e798ce71ee25773654694dc89
caio-faustino <caioexfaustino.pro@gmail.com>
2022-11-09 20:19:48 -0300

    add menu and dashboard from admin

--- client/src/pages/admin/dashboard/index.js
+++ client/src/pages/admin/dashboard/index.js
@@ -1,11 +1,49 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function Dashboard() {
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+// import Paper from '@mui/material/Paper';
+import MenuAdmin from '../../../components/menu-admin';
+import ImgAdmin from '../../../assets/img/admin.png';
+import Footer from '../../../components/footer-admin';
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+
+        <MenuAdmin />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              <img src={ImgAdmin} />
+            </Grid>
+            <Footer sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+
+      </Box>
+    </ThemeProvider>
   );
 }
 
-
+export default function Dashboard() {
+return <DashboardContent />;
+}
